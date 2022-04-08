7ccbd350e8c16a33a1eb2b42f68a274f62a2676c
Pedro Cavalero <pedro.cavalero@gmail.com>
2022-04-08 17:45:50 -0300

    Adding Redis cache

--- cache.py
+++ cache.py
@@ -1,12 +1,25 @@
 from flask import Flask, request, jsonify
 import requests
 from flask_caching import Cache
+from functools import wraps
+import datetime
 
 app = Flask(__name__)
 app.config.from_object('config.BaseConfig')
 cache = Cache(app)
 
+def time(function=None):
+    @wraps(function)
+    def wrapper(*args, **kwargs):
+        s = datetime.datetime.now()
+        _ = function(*args, **kwargs)
+        e = datetime.datetime.now()
+        app.logger.info("Execution Time : {} ".format(e-s))
+        return _
+    return wrapper
+
 @app.route("/universities")
+@time
 @cache.cached(timeout=30, query_string=True)
 def get_universities():
     API_URL = "http://universities.hipolabs.com/search?country="
